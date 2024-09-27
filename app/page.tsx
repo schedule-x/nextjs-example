ff3bd07a490158f4262fc52c38cdcdbef6012112
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2024-09-27 07:24:17 +0200

    use new plugin api

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,27 @@ import '@schedule-x/theme-default/dist/index.css'
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
 import {createEventModalPlugin} from "@schedule-x/event-modal";
 import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
+import {createEventsServicePlugin} from "@schedule-x/events-service";
+import {useState} from "react";
 
 export default function Home() {
+  const eventsServicePlugin = useState(() => createEventsServicePlugin())[0];
+
   const calendarApp = useCalendarApp({
     views: [viewWeek, viewMonthGrid, viewDay, viewMonthAgenda],
     defaultView: viewWeek.name,
+    callbacks: {
+      onRangeUpdate: (range) => {
+        calendarApp.eventsService.set([
+          {
+            id: '12',
+            title: 'Event 1',
+            start: range.start,
+            end: range.end,
+          },
+        ])
+      }
+    },
     events: [
       {
         id: '12',
@@ -24,8 +40,7 @@ export default function Home() {
       },
     ],
     selectedDate: '2023-12-15',
-    plugins: [createDragAndDropPlugin(), createEventModalPlugin()],
-  })
+  }, [createDragAndDropPlugin(), createEventModalPlugin(), eventsServicePlugin])
 
   return (
     <main className={styles.main}>
